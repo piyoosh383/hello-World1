8853ef6a216cf0393e93aaade0ae28905640a877
piyoosh383 <84227633+piyoosh383@users.noreply.github.com>
2021-05-17 16:01:58 +0530

    Add files via upload

---
+++ TestGithub.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+
+    $scope.GetData = function () {
+        $http
+            ({
+                method: 'GET',
+                url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",
+                header: {
+                    "Accept": "application/json;odata=verbose"
+                }
+            }).then(function (getdata) {
+                console.log(myData);
+                $scope.myData = getdata.data;
+                // console.log($scope.groupBY); 
+            },
+                function (error) {
+                    console.log(error);
+                });
+    }
+});
