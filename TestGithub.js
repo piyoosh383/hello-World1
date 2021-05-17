eaa3d6fa8df6184b7f7e4e1cc6d6af4f9eab6096
piyoosh383 <84227633+piyoosh383@users.noreply.github.com>
2021-05-17 18:20:07 +0530

    Update TestGithub.js

--- TestGithub.js
+++ TestGithub.js
@@ -1,23 +1,22 @@
-var app = angular.module('myApp', []);
-app.controller('myCtroller', function ($scope, $http) {
+ShareApp.controller("myCtrl", ["$scope", "$q", "$http", "$rootScope", "$uibModal", "$timeout", "SharewebListService", "SharewebCommonFactoryService", "GlobalConstants",
+    function ($scope, $q, $http, $rootScope, $uibModal, $timeout, SharewebListService, SharewebCommonFactoryService, GlobalConstants) {
 
-    $scope.GetData = function () {
-        $http
-            ({
-                method: 'GET',
-                url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",                      
-            headers: {
-                "accept": "application/json;odata=verbose",
-                "content-Type": "application/json;odata=verbose"
-            }
-            }).then(function (getdata) {
-                console.log(myData);
-                $scope.myData = getdata.data;
-                // console.log($scope.groupBY); 
-            },
-                function (error) {
-                    console.log(error);
-                });
-    }
-    $scope.GetData();
-});
+        $scope.GetData = function () {
+            $http
+                ({
+                    method: 'GET',
+                    url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",
+                    header: {
+                        "Accept": "application/json;odata=verbose"
+                    }
+                }).then(function (getdata) {
+                    console.log(myData);
+                    $scope.myData = getdata.data;
+                    // console.log($scope.groupBY); 
+                },
+                    function (error) {
+                        console.log(error);
+                    });
+        }
+        $scope.GetData();
+    }]);
