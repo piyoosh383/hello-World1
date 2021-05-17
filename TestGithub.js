259ada46d14b40e012eae95d01be3d4718d5fcd4
piyoosh383 <84227633+piyoosh383@users.noreply.github.com>
2021-05-17 18:35:56 +0530

    Update TestGithub.js

--- TestGithub.js
+++ TestGithub.js
@@ -8,6 +8,7 @@ app.controller('myCtrl', function ($scope, $http) {
                 url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",
                 header: {
                     "Accept": "application/json;odata=verbose"
+                    "Access-Control-Allow-Origin": "*"
                 }
             }).then(function (getdata) {
                 console.log(myData);
