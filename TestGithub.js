fdae187afdf4820ca074638c72b095c03cdea41a
piyoosh383 <84227633+piyoosh383@users.noreply.github.com>
2021-05-17 18:20:46 +0530

    Update TestGithub.js

--- TestGithub.js
+++ TestGithub.js
@@ -1,22 +1,22 @@
-ShareApp.controller("myCtrl", ["$scope", "$q", "$http", "$rootScope", "$uibModal", "$timeout", "SharewebListService", "SharewebCommonFactoryService", "GlobalConstants",
-    function ($scope, $q, $http, $rootScope, $uibModal, $timeout, SharewebListService, SharewebCommonFactoryService, GlobalConstants) {
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
 
-        $scope.GetData = function () {
-            $http
-                ({
-                    method: 'GET',
-                    url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",
-                    header: {
-                        "Accept": "application/json;odata=verbose"
-                    }
-                }).then(function (getdata) {
-                    console.log(myData);
-                    $scope.myData = getdata.data;
-                    // console.log($scope.groupBY); 
-                },
-                    function (error) {
-                        console.log(error);
-                    });
-        }
-        $scope.GetData();
-    }]);
+    $scope.GetData = function () {
+        $http
+            ({
+                method: 'GET',
+                url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",
+                header: {
+                    "Accept": "application/json;odata=verbose"
+                }
+            }).then(function (getdata) {
+                console.log(myData);
+                $scope.myData = getdata.data;
+                // console.log($scope.groupBY); 
+            },
+                function (error) {
+                    console.log(error);
+                });
+    }
+    $scope.GetData();
+});
